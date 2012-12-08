174b80c36528948eb99f42476b0f48682044ad50
Thomas Anderson <tandeson@gmail.com>
2012-12-08 11:57:10 -0800

    Refactoring of code:
    - Moved to using arrays to init the motors.
    - Moved to using loops to setup the hardware
    - Changed a few names
    - Reduced the number of 'magic' numbers (e.g. numbers which didn't make it clear how they were calculated).

--- countdown.ino
+++ countdown.ino
@@ -1,74 +1,77 @@
-#include <Servo.h> 
-#define start 180
+#include <Servo.h>
 
-Servo digitone;
-Servo digittwo;
-Servo digithour;
-int postionone = start;
-int postiontwo = start;
-int postionhour = start;
+// values for SERVO can be from 0 to 180
+#define SERVO_RANGE 180
+
+// number of servos in system
+#define NUMBER_OF_POSITIONS 3
+
+// pins the srvos are located on
+const int SERVO_IO_LOCATIONS[NUMBER_OF_POSITIONS] = {11, 10, 9};
+
+// pointer to servo objects, used of control servos
+Servo* pServo[NUMBER_OF_POSITIONS];
+
+//Data on where to place the servos
+int servo_current_locations[NUMBER_OF_POSITIONS] = {SERVO_RANGE,SERVO_RANGE,SERVO_RANGE};
+
 unsigned long pasttime = 0;
 unsigned long currenttime = millis();
 int finished = 0;
 
-
-
 void setup() {
- 
- // attaching servos 
-digitone.attach(11);
-digittwo.attach(10);
-digithour.attach(9);
+  for(int i = 0 ; i < NUMBER_OF_POSITIONS; i++)
+  {
+      // Create a new Servo object, and keep a pointer to it.
+      pServo[i] = new Servo;
 
-//sets up intial countdown
-digitone.write(postionone);
-digittwo.write(postiontwo);
-digithour.write(postionhour);
+      // User the pointer to attach a control line to the servo.
+      pServo[i]->attach(SERVO_IO_LOCATIONS[i]);
 
+      // Write the default start position to the servo.
+      pServo[i]->write(servo_current_locations[i]);
+  }
 }
-
 // increases digit one and hour servo angle, 9 postions
 int posincrone(int x) {
   int y;
-  y = x - 20;
+  y = x - ( SERVO_RANGE / 9 );
   return y;
 }
 // increases digit two servo angle, 5 postions
 int posincrtwo(int x) {
   int y;
-  y = x - 36;
+  y = x - ( SERVO_RANGE / 5);
   return y;
 }
 
 void loop() {
-  currenttime = millis();
+  unsigned long currenttime = millis();
   
     // checks to see if 1 min has gone by.
   if (currenttime - pasttime > 60000 || finished != 1) {
-    pasttime = currenttime;
+      pasttime = currenttime;
     
-    // if the digitone is not in the zeros postion it will move it one postion down
-      if (postionone >= 0) {
-      digitone.write(postionone);
-      postionone = posincrone(postionone);
+    // if the servo 1 is not less then zeros it will move it one postion down
+      if (servo_current_locations[0] >= 0) {
+        servo_current_locations[0] = posincrone(servo_current_locations[0]);
+        pServo[0]->write(servo_current_locations[0]);
       
-    // if it is in the zero postion, the next servo, digittwo, will move one postion, and reset digitone to 180 degrees
-      }else {
-        postionone = start;
-        digittwo.write(postiontwo);
-        postionone = posincrtwo(postiontwo);
-            if (postiontwo >= 0) {
-            digithour.write(postionhour);
-            postionhour = posincrone(postionhour);
-        } else { 
-            postiontwo = start;
-        }
-    
-    }
+    // if servo 1 is less then 0 it will reset it to 180 and move servo 2 one postion down 
+      }else if (servo_current_locations[0] < 0 || servo_current_locations[1] >= 0 ) {
+        servo_current_locations[0] = SERVO_RANGE;
+        servo_current_locations[1] = posincrtwo(servo_current_locations[1]);
+        pServo[1]->write(servo_current_locations[1]);
+        
+     // if servo 2 is less then 0 it will reset it to 180 and move servo 3 one postion down
+      }else if (servo_current_locations[1] < 0) {
+        servo_current_locations[1] = SERVO_RANGE;  
+        servo_current_locations[2] = posincrone(servo_current_locations[3]);
+        pServo[2]->write(servo_current_locations[2]);
   }
   // checks to see if countdown has finished
-   if (postionhour == 0 || postiontwo == 0 || postionone ==0) {
+   if (servo_current_locations[2] == 0 || servo_current_locations[1] == 0 || servo_current_locations[0] ==0) {
      finished = 1;
+    }
+  }
 }
-}
-
